9a5352c1f1e484188eee97f52c21b05ab1d05c6c
Your Name <fransofloo@gmail.com>
2021-06-01 17:07:28 -0500

    get respuestas.php

--- Respuesta.php
+++ Respuesta.php
@@ -1,7 +1,3 @@
 <?php
-    $nombres= $_GET['nombres'];
-    $apellidos= $_GET['apellidos'];
-    $curso= $_GET['curso'];
-    $edad= $_GET['edad'];
-    $contraseña= $_GET['contraseña'];
+    print_r($_GET)
 ?>
